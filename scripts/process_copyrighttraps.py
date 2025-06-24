7f282e036e890d4b1e9bf495c46a226ddcf5d29a
Your Name <abha4861@BUS-WYCXY33-L.local>
2025-06-24 06:07:26 -0600

    k

--- scripts/process_copyrighttraps.py
+++ scripts/process_copyrighttraps.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 
 blocksbin = pd.read_csv("copywrite_traps_blocksbin.csv")
 blocksbin = blocksbin.rename(columns={"score": "score_out_of_sample"})
@@ -15,5 +16,10 @@ R = R[["method", "delta", "doc_id"]].drop_duplicates()[["method", "delta"]].grou
 print(R)
 
 
-
-print(noblocksbin["score_in_sample"].mean(), blocksbin["score_out_of_sample"].mean(), zeros["score_zero"].mean())
+with open("copywritetraps.csv", "w") as of:
+	onethousands = math.log(noblocksbin["score_in_sample"].mean())
+	fivehundreds = math.log(blocksbin["score_out_of_sample"].mean())
+	zeros = math.log(zeros["score_zero"].mean())
+	of.write(f"{1000},{onethousands}\n")
+	of.write(f"{500},{fivehundreds}\n")
+	of.write(f"{0},{zeros}\n")
