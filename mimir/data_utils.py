e7fd0122ee8447f02b07086ef8c08e20bf345290
Your Name <abha4861@cu-vpn-leeds-172.21.36.17.int.colorado.edu>
2025-06-23 21:02:01 -0600

    k

--- mimir/data_utils.py
+++ mimir/data_utils.py
@@ -122,6 +122,8 @@ class Data:
             ds = datasets.load_dataset(self.name)["train"].shuffle(seed=42)
 
             if self.name == "abehandlerorg/copywritetraps":
+                # strip the " at start and end
+                ds = ds.map(lambda x: {"text": x["text"].strip('"')})
                 return ds
 
             #some of these examples do not have urls. why?
