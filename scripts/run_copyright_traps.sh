04eadf05bc5fd4bbaa1d290a033a5eb5485ac406
Your Name <abha4861@cu-vpn-leeds-172.21.36.88.int.colorado.edu>
2025-06-24 12:22:23 -0600

    k

--- scripts/run_copyright_traps.sh
+++ scripts/run_copyright_traps.sh
@@ -1,6 +1,6 @@
 gpu=$1
 gpui=$((gpu))
-export CUDA_VISIBLE_DEVICES=$gpui && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/copywrite_traps_blocksbin.json
+export CUDA_VISIBLE_DEVICES=$gpui && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/copywrite_traps_zeros.json
 export CUDA_VISIBLE_DEVICES=$gpui && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/copywrite_traps_blocksbin.json
 export CUDA_VISIBLE_DEVICES=$gpui && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/copywrite_traps_noblocksbin.json
 conda run --live-stream -n analysis python build_output.py --config copywrite_traps_noblocksbin
