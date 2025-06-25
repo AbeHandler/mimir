1460ccfe14510ae9adf6dbf3857445ee8a132fd3
Your Name <abha4861@cu-vpn-leeds-172.21.36.30.int.colorado.edu>
2025-06-25 11:49:16 -0600

    k

--- locals.py
+++ locals.py
@@ -1,6 +1,13 @@
 from datasets import load_dataset, Dataset
 import os
 
+from pathlib import Path
+
+token_path = Path.home() / ".cache" / "huggingface" / "token_write"
+with open(token_path, "r") as f:
+    token = f.read().strip()
+
+
 # Load your dataset (adjust as needed)
 ds = load_dataset("abehandlerorg/blockeddocs", split="train")
 
@@ -9,3 +16,8 @@ filtered_ds = ds.filter(lambda x: "/local/" in x["url"])
 
 # Save using Hugging Face's internal format
 filtered_ds.save_to_disk("localdocs")
+
+
+from datasets import DatasetDict
+dataset = load_from_disk("localdocs")
+dataset.push_to_hub("abehandlerorg/localblockeddocs", token=token)
