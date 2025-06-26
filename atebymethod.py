01a05a5237ec11c889df743d3154cf00f8496a89
Your Name <abha4861@cu-vpn-leeds-172.21.36.77.int.colorado.edu>
2025-06-25 22:11:08 -0600

    k

---
+++ atebymethod.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import altair as alt
+
+stats = pd.read_csv("stats.csv")
+noblocks = pd.read_csv("minhashblocksample_noblocks.lite.csv").rename(columns={"score": "noblocks_score"})
+noblocks = stats.merge(noblocks, left_on='url', right_on="doc_id").drop(columns=['size'])
+stats = pd.read_csv("stats.csv")
+blocks = pd.read_csv("minhashblocksample_blocks.lite.csv").rename(columns={"score": "blocks_score"})
+blocks = stats.merge(blocks, left_on='url', right_on="doc_id")
+
+method = "min_k"
+D = blocks.merge(noblocks, on=["doc_id", "method"])[["blocks_score", "size", "noblocks_score", "doc_id", "method"]].drop_duplicates()
+D["delta"] = D["blocks_score"] - D["noblocks_score"]
+D = D[D["size"] <= 30].copy()
+D["size_bin"] = pd.cut(D["size"], bins=range(0, 31, 3), right=True)
+D = D[D['method'] == method].copy()
+
+df = D.groupby("size_bin", observed=True).agg(
+    delta_mean=("delta", "mean"),
+    count=("delta", "count")
+).reset_index()
+
+
+df["size_bin"] = df["size_bin"].astype(str)
+chart = alt.Chart(df).mark_bar().encode(
+    x=alt.X("size_bin:N", sort=None, title="Count of similar documents in BlockBench"),
+    y=alt.Y("delta_mean:Q", title="Mean ATE"),
+    tooltip=["size_bin", "delta_mean", "count"]
+).properties(
+    title=f"Mean ATE by Repetition: {method}",
+    width=400,
+    height=300
+)
+
+chart.save(f"{method}.html")
