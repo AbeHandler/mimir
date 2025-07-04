d7843cb35fb32803431aa56f828f9796ece69a19
Your Name <abha4861@cu-vpn-leeds-172.21.36.17.int.colorado.edu>
2025-07-04 08:52:09 -0600

    k

--- scripts/run_twfe.sh
+++ scripts/run_twfe.sh
@@ -1,4 +1,4 @@
-gpu=$0
+gpu=0
 gpui=$((gpu))
 export CUDA_VISIBLE_DEVICES=$gpui && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/twfe_blocks.lite.json
 export CUDA_VISIBLE_DEVICES=$gpui && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/twfe_noblocks.lite.json
