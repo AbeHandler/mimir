043d7b09602cbcfa811fff694f0471ddd60e0d29
abe <abram.handler@gmail.com>
2025-07-08 02:55:47 +0000

    k

--- scripts/run_twfe.sh
+++ scripts/run_twfe.sh
@@ -1,3 +1,4 @@
+set -e
 gpu=0
 gpui=$((gpu))
 export CUDA_VISIBLE_DEVICES=$gpui && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/twfe_blocks.lite.json
