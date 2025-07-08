97207042c9d8501d4ec90a0885d14816dab26202
Your Name <abha4861@cu-vpn-leeds-172.21.36.110.int.colorado.edu>
2025-07-08 10:25:14 -0600

    k

--- scripts/run_heavy_gpu_mia_methods.sh
+++ scripts/run_heavy_gpu_mia_methods.sh
@@ -1,5 +1,6 @@
 set -e
-gpu=0
-gpui=$((gpu))
-export CUDA_VISIBLE_DEVICES=$gpui && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/minhashblocksample_blocks.heavy.json
-export CUDA_VISIBLE_DEVICES=$gpui && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/minhashblocksample_noblocks.heavy.json
+export CUDA_VISIBLE_DEVICES=0,1 && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/minhashblocksample_blocks.heavy.json
+export CUDA_VISIBLE_DEVICES=0,1 && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/minhashblocksample_noblocks.heavy.json
+
+conda run --live-stream -n analysis python build_output.py --config minhashblocksample_noblocks.heavy
+conda run --live-stream -n analysis python build_output.py --config minhashblocksample_blocks.heavy
