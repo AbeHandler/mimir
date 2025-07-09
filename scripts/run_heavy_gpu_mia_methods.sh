3f8ca40ef8b24a06b18c30e1b6ea30acfb09fac2
Your Name <abha4861@cu-vpn-leeds-172.21.36.67.int.colorado.edu>
2025-07-09 09:36:27 -0600

    k

--- scripts/run_heavy_gpu_mia_methods.sh
+++ scripts/run_heavy_gpu_mia_methods.sh
@@ -2,5 +2,5 @@ set -e
 export CUDA_VISIBLE_DEVICES=0,1 && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/minhashblocksample_blocks.heavy.json
 export CUDA_VISIBLE_DEVICES=0,1 && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/minhashblocksample_noblocks.heavy.json
 
-conda run --live-stream -n analysis python build_output.py --config minhashblocksample_noblocks.heavy
-conda run --live-stream -n analysis python build_output.py --config minhashblocksample_blocks.heavy
+time conda run --live-stream -n analysis python build_output.py --config minhashblocksample_noblocks.heavy
+time conda run --live-stream -n analysis python build_output.py --config minhashblocksample_blocks.heavy
