98e928068f3123ee778a2be031913640e3a09d86
Your Name <abha4861@cu-vpn-leeds-172.21.36.50.int.colorado.edu>
2025-07-10 08:56:20 -0600

    k

---
+++ scripts/run_zlib.sh
@@ -0,0 +1,6 @@
+set -e
+export CUDA_VISIBLE_DEVICES=0 && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/minhashblocksample_blocks.zlib.json
+export CUDA_VISIBLE_DEVICES=1 && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/minhashblocksample_noblocks.heavy.json
+
+time conda run --live-stream -n analysis python build_output.py --config minhashblocksample_noblocks.heavy
+time conda run --live-stream -n analysis python build_output.py --config minhashblocksample_blocks.heavy
