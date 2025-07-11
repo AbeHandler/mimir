b07fb7c3e70e4f9feda514613487fc33e67745b1
Your Name <abha4861@cu-vpn-leeds-172.21.36.125.int.colorado.edu>
2025-07-10 23:22:17 -0600

    k

--- scripts/run_zlib_natural_traps.sh
+++ scripts/run_zlib_natural_traps.sh
@@ -1,2 +1,8 @@
 export CUDA_VISIBLE_DEVICES=1 && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/suffixesblocksbin.zlib.json
 export CUDA_VISIBLE_DEVICES=0 && MIMIR_DATA_SOURCE=mimirdata MIMIR_CACHE_PATH=mimrcache conda run --live-stream -n mimir python run.py --config configs/suffixesnoblocksbin.zlib.json
+
+conda run --live-stream -n analysis python build_output.py --config suffixesnoblocksbin.zlib
+conda run --live-stream -n analysis python build_output.py --config suffixesblocksbin.zlib
+
+mv suffixesblocksbin.zlib.csv csvs
+mv suffixesnoblocksbin.zlib.csv csvs
