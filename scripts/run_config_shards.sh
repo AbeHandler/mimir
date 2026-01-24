16bbfee613b9f9907a9d3eaa9181e3f34d930121
Your Name <abram.handler@gmail.com>
2026-01-24 13:53:03 -0700

    shards

--- scripts/run_config_shards.sh
+++ scripts/run_config_shards.sh
@@ -6,7 +6,7 @@ GPU_NUMBER="${1:-0}"  # Default to GPU 0 if not specified
 # real line is here ==> for CONFIG_FILENAME in "excluded-docs.blocks.clipped.json" "excluded-docs.noblocks.clipped.json" "bothbins.blocks.lite.json" "bothbins.noblocks.lite.json" "excluded-docs.blocks.lite.json" "excluded-docs.noblocks.lite.json" "confounddataset.blocks.lite.json" "confounddataset.noblocks.lite.json" "matching_neighbors.blocks.lite.json"; do
 
 # express line below for now 1/7/2025
-for CONFIG_FILENAME in "excluded-docs.blocks.bisection.json" "excluded-docs.noblocks.bisection.json"; do
+for CONFIG_FILENAME in "excluded-docs.blocks.dcpdd.json" "excluded-docs.noblocks.dcpdd.json"; do
     echo "Processing config: $CONFIG_FILENAME"
 
     # Use 20 shards for excluded-docs and bothbins, 60 for matching_neighbors, 36 for others
