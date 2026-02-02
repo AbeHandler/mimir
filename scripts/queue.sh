2f404028b96b5a2b829a39a4865cbeccacfa9d95
Your Name <abram.handler@gmail.com>
2026-02-01 20:25:56 -0700

    k

--- scripts/queue.sh
+++ scripts/queue.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 
-./scripts/run_config_gpt_oss.sh gptoss.20b.2024-07-30-to-2024-07-30-Y1.lite.json
+./scripts/run_config_gpt_oss.sh gptoss.20b.2024-07-30-to-2024-07-30-Y1.excluded.lite.json
 
-./scripts/run_config_gpt_oss.sh gptoss.20b.2024-07-30-to-2024-07-30-Y0.lite.json
+./scripts/run_config_gpt_oss.sh gptoss.20b.2024-07-30-to-2024-07-30-Y0.excluded.lite.json
 
 ./scripts/run_config_gpt_oss.sh gptoss.20b.2024-07-30-to-2024-07-30-Y0.bothbins.lite.json
 
