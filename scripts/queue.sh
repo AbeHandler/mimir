23f044c36e19c603ba5c2e02b922d16e87afbf2f
Your Name <abram.handler@gmail.com>
2026-02-01 19:37:22 -0700

    k

--- scripts/queue.sh
+++ scripts/queue.sh
@@ -5,4 +5,8 @@
 
 ./scripts/run_config_gpt_oss.sh gptoss.20b.2024-07-30-to-2024-07-30-Y0.lite.json
 
+./scripts/run_config_gpt_oss.sh gptoss.20b.2024-07-30-to-2024-07-30-Y0.bothbins.lite.json
+
+./scripts/run_config_gpt_oss.sh gptoss.20b.2024-07-30-to-2024-07-30-Y1.bothbins.lite.json
+
 ./scripts/run_single_publishers.sh
