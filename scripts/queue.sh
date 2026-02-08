9c999e0399dd8f5aee824250637c577acde10356
Your Name <abram.handler@gmail.com>
2026-02-08 16:05:47 -0700

    k

--- scripts/queue.sh
+++ scripts/queue.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 
-./scripts/run_config_llama.sh Llama-3.1-8B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y0.excluded.lite.json
-./scripts/run_config_llama.sh Llama-3.1-8B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y1.excluded.lite.json
-
 ./scripts/run_config_llama.sh Llama-3.1-8B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y0.bothbins.lite.json
 ./scripts/run_config_llama.sh Llama-3.1-8B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y1.bothbins.lite.json
 
 ./scripts/run_single_publishers.sh
 
 exit 0
+
+./scripts/run_config_llama.sh Llama-3.1-8B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y0.excluded.lite.json
+./scripts/run_config_llama.sh Llama-3.1-8B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y1.excluded.lite.json
 
 ./scripts/run_config_shards.sh 0
 
