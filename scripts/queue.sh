969fb4b27d8b8be2b3983300500c56844ec4c0bd
Your Name <abram.handler@gmail.com>
2026-02-17 06:35:48 -0700

    k

--- scripts/queue.sh
+++ scripts/queue.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 
-./scripts/run_config_llama.sh Llama-3.1-70B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y0.bothbins.lite.json
-./scripts/run_config_llama.sh Llama-3.1-70B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y1.bothbins.lite.json
-./scripts/run_config_llama.sh Llama-3.1-70B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y0.excluded.lite.json
-./scripts/run_config_llama.sh Llama-3.1-70B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y1.excluded.lite.json
+./scripts/run_config_llama.sh Llama-3.3-70B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y0.bothbins.lite.json
+./scripts/run_config_llama.sh Llama-3.3-70B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y1.bothbins.lite.json
+./scripts/run_config_llama.sh Llama-3.3-70B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y0.excluded.lite.json
+./scripts/run_config_llama.sh Llama-3.3-70B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y1.excluded.lite.json
 
 ./scripts/run_config_llama.sh Llama-3.1-8B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y0.bothbins.lite.json
 ./scripts/run_config_llama.sh Llama-3.1-8B-Instruct-bnb-4bit_cptllama-2024-01-30-to-2024-01-30-Y1.bothbins.lite.json
